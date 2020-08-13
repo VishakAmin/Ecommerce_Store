bafde77a9273fd3dd2df6146705a47a5aeb3a3fe
VishakAmin <aminvishak@gmail.com>
2020-08-13 18:47:35 +0530

    Updated Backened API

--- projfrontend/src/auth/helper/index.js
+++ projfrontend/src/auth/helper/index.js
@@ -1,7 +1,7 @@
 import { API } from "../../backend"
 
 export const signup = user => {
-    return fetch(`${API}signup`, {
+    return fetch(`${API}/signup`, {
         method: "POST",
         headers: {
             Accept: "application/json",
